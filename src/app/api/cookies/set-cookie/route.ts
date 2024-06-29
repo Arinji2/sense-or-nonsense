c4114572aaa039cdb684da86addd002ce9db6905
Arinji2 <arinjaydhar205@gmail.com>
2024-06-29 20:24:54 +0530

    Add Name for Cookie Route

--- src/app/api/cookies/set-cookie/route.ts
+++ src/app/api/cookies/set-cookie/route.ts
@@ -2,9 +2,10 @@ export async function POST(request: Request) {
   const res = await request.json();
 
   if (!res.token) throw new Error("No token provided for Set Cookie Endpoint");
+  if (!res.name) throw new Error("No name provided for Set Cookie Endpoint");
 
   return new Response("Success", {
     status: 200,
-    headers: { "Set-Cookie": `token=${res.token}` },
+    headers: { "Set-Cookie": `${res.name}=${res.token}` },
   });
 }
