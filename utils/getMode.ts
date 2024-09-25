50b0e64d5727443f8bdadc689649d41ef58d5b52
Arinji2 <arinjaydhar205@gmail.com>
2024-09-25 18:32:41 +0530

    Return a pb client from user mode

--- utils/getMode.ts
+++ utils/getMode.ts
@@ -1,22 +1,45 @@
 import { GuestToUser } from "@/actions/guest";
+
 import { cookies } from "next/headers";
-import { ConnectPBUser } from "./connectPB";
+import Client from "pocketbase";
+import { ConnectPBAdmin, ConnectPBUser } from "./connectPB";
 
 export async function GetUserMode() {
   const cookieStore = cookies();
-  let userID: string | null = null;
+  let globalUserID: string | null = null;
   let mode: "guest" | "user" | null = null;
+  let pb: Client | null = null;
   if (cookieStore.get("user") !== undefined) {
     const pbUser = await ConnectPBUser();
-    if (pbUser.authStore.isValid) {
-      userID = pbUser.authStore.model!.id as string;
+    if (pbUser.pb.authStore.isValid) {
+      globalUserID = pbUser.pb.authStore.model!.id as string;
       mode = "user";
+      pb = pbUser.pb;
     }
   } else if (cookieStore.get("guest-session") !== undefined) {
-    const id = await GuestToUser();
-    userID = id;
+    const { userID, guestID } = await GuestToUser();
+    globalUserID = userID;
     mode = "guest";
+    pb = await ConnectPBAdmin();
   }
 
-  return { userID, mode };
+  if (!mode) {
+    return {
+      userID: null,
+      mode: null,
+      pb: null,
+    };
+  }
+
+  if (!pb) {
+    throw new Error("User not logged in");
+  }
+
+  pb.autoCancellation(false);
+
+  return {
+    userID: globalUserID,
+    mode,
+    pb,
+  };
 }
