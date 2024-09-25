becf8e461db688f79f9b5c1c4a547c97011e1c18
Arinji2 <arinjaydhar205@gmail.com>
2024-09-25 19:15:41 +0530

    Add Game Routes

---
+++ constants/game-routes.ts
@@ -0,0 +1,12 @@
+//Routes here are used in src/app/game-in-progress.tsx to check if the user is in the game mode. If not we show a continue to game modal
+
+export const GAME_ROUTES = [
+  "/single",
+  "/multi",
+  "/backdrop",
+  "/difficulty",
+  "/fighters",
+  "/instructions",
+  "/pregame",
+  "/game",
+];
