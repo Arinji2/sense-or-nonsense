7a927decb36b04c12cb8c5bf1422054df7decfef
Arinji2 <arinjaydhar205@gmail.com>
2024-09-26 13:22:49 +0530

    Fix UI Issues

--- src/app/pregame/fighters.tsx
+++ src/app/pregame/fighters.tsx
@@ -24,11 +24,11 @@ export default function Fighters({
         </p>
         <EditButton objKey="playerData" />
       </div>
-      <div className="no-scrollbar flex h-fit w-full min-w-0 max-w-[300px] flex-row flex-wrap items-center justify-center gap-4 overflow-x-auto xl:flex-nowrap xl:justify-start">
+      <div className="no-scrollbar flex h-fit w-full flex-row flex-wrap items-center justify-center gap-4 overflow-x-auto rounded-sm xl:max-w-[300px] xl:flex-nowrap xl:justify-start">
         {fighters.map((fighter, index) => (
           <div
             key={fighter.id + index}
-            className="flex h-fit w-full flex-col items-center justify-center rounded-sm bg-gradient-to-r from-[--fighterColor] from-[60%] to-[--fighterColorOpaque] px-4 py-6 xl:max-w-[500px]"
+            className="flex h-fit w-full flex-col items-center justify-center rounded-sm bg-gradient-to-r from-[--fighterColor] from-[60%] to-[--fighterColorOpaque] px-5 py-6 xl:w-[250px] xl:shrink-0"
             style={
               {
                 "--fighterColor": fighter.color,
@@ -37,7 +37,7 @@ export default function Fighters({
             }
           >
             <div className="flex h-full w-full flex-row items-end justify-between xl:pr-10">
-              <div className="flex size-[50px] shrink-0 flex-col items-start justify-center xl:size-[100px]">
+              <div className="flex size-[60px] shrink-0 flex-col items-start justify-center xl:size-[100px]">
                 <div className="relative h-full w-full overflow-hidden rounded-sm">
                   <Image
                     alt={fighter_data![index].fighter_name ?? "Unknown"}
@@ -48,9 +48,9 @@ export default function Fighters({
                   />
                 </div>
               </div>
-              <div className="flex h-full w-fit min-w-0 flex-col items-start justify-end gap-4">
+              <div className="flex h-full w-fit min-w-0 flex-col items-start justify-end gap-4 pl-2 xl:ml-auto">
                 <h4 className="w-full truncate text-left text-[15px] font-bold text-white md:text-[20px]">
-                  {fighter_data![index].fighter_name}{" "}
+                  {fighter_data![index].fighter_name}
                 </h4>
                 <p className="whitespace-nowrap text-[15px] text-white">
                   PLAYER {index + 1}
--- src/app/pregame/page.tsx
+++ src/app/pregame/page.tsx
@@ -31,28 +31,30 @@ export default async function Page() {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-start">
       <WidthWrapper>
-        <div className="relative flex w-full flex-col items-center justify-start gap-12 pb-10 xl:h-[100svh]">
+        <div className="relative flex w-full flex-col items-center justify-center gap-12 pb-10 xl:h-[100svh]">
           <h1 className="w-full px-2 text-center text-[35px] font-bold tracking-subtitle text-white md:text-[40px] xl:text-[60px] xl:leading-[100px]">
             CONFIRM YOUR SELECTIONS
           </h1>
-          <div className="flex h-fit w-full flex-col items-center justify-center gap-6 xl:flex-row xl:gap-20">
-            <div className="flex h-fit w-[80%] flex-col items-start justify-start gap-6 xl:w-fit">
-              <Game gameID={gameID!} />
-              <Fighters fighter_data={fighterData!} />
+          <div className="flex h-fit w-full flex-col-reverse items-center justify-start gap-12 xl:flex-col">
+            <div className="flex h-fit w-full flex-col items-center justify-center gap-6 xl:flex-row xl:gap-20">
+              <div className="flex h-fit w-[80%] flex-col items-start justify-start gap-6 xl:w-fit">
+                <Game gameID={gameID!} />
+                <Fighters fighter_data={fighterData!} />
+              </div>
+              <div className="flex h-fit w-[80%] flex-col items-start justify-start gap-6 xl:w-fit">
+                <Backdrop backdropID={backdrop!} />
+                <Difficulty difficultyID={difficulty!} />
+              </div>
             </div>
-            <div className="flex h-fit w-[80%] flex-col items-start justify-start gap-6 xl:w-fit">
-              <Backdrop backdropID={backdrop!} />
-              <Difficulty difficultyID={difficulty!} />
+            <div className="flex h-fit w-[80%] flex-row flex-wrap items-center justify-center gap-6 px-2">
+              <GameSetup />
+              <Link
+                href="/pregame/instructions"
+                className="flex h-fit w-full shrink-0 scale-105 flex-col items-center justify-center rounded-md bg-purple-500 p-2 text-[15px] text-white transition-transform duration-200 ease-in-out will-change-transform hover:scale-100 xl:w-fit xl:p-4 xl:text-[20px]"
+              >
+                HOW TO PLAY
+              </Link>
             </div>
-          </div>
-          <div className="flex h-fit w-[80%] flex-row flex-wrap items-center justify-center gap-6 px-2">
-            <GameSetup />
-            <Link
-              href="/pregame/instructions"
-              className="flex h-fit w-full shrink-0 scale-105 flex-col items-center justify-center rounded-md bg-purple-500 p-2 text-[15px] text-white transition-transform duration-200 ease-in-out will-change-transform hover:scale-100 xl:w-fit xl:p-4 xl:text-[20px]"
-            >
-              HOW TO PLAY
-            </Link>
           </div>
         </div>
       </WidthWrapper>
