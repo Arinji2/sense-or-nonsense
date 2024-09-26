ba61a42cb808558901e2d3dea38ef10ffbee6a95
Arinji2 <arinjaydhar205@gmail.com>
2024-09-26 11:55:38 +0530

    Remove unused schemas

--- validations/game-data/schema.ts
+++ validations/game-data/schema.ts
@@ -1,63 +1,9 @@
 import z from "zod";
-import { GameFighterSchemaType, RoundsSchemaType } from "./types";
 export const GameFighterSchema = z.object({
   fighter_id: z.number(),
   fighter_name: z.string(),
 });
 
-export const RoundsSchema = z.object({
-  round: z.number(),
-  playerIndex: z.number(),
-  recordID: z.string(),
-  isCorrect: z.boolean(),
-  timeElapsed: z.number(),
-  isFake: z.boolean(),
-});
-
-export const RoundsSchemaWithWords = RoundsSchema.extend({
-  word: z.string(),
-});
-
-export const GameDataSchema = z.object({
-  game_id: z
-    .string()
-
-    .optional()
-    .transform((val) => {
-      if (!val) return undefined;
-
-      return val;
-    }),
-
-  fighter_data: z
-    .string()
-    .or(z.array(GameFighterSchema))
-    .optional()
-    .transform((val) => {
-      if (typeof val === "string") {
-        return JSON.parse(val) as GameFighterSchemaType[];
-      } else if (Array.isArray(val)) {
-        return val as GameFighterSchemaType[];
-      }
-    }),
-  difficulty: z.string().optional(),
-  backdrop: z.string().optional(),
-  game: z
-    .string()
-    .or(z.array(RoundsSchema))
-    .optional()
-    .transform((val) => {
-      if (typeof val === "string") {
-        return JSON.parse(val) as RoundsSchemaType[];
-      } else if (Array.isArray(val)) {
-        return val as RoundsSchemaType[];
-      }
-    }),
-});
-
-export const GameDataSchemaWithWords = GameDataSchema.extend({
-  game: z.array(RoundsSchemaWithWords),
-});
 export const WordSchema = z.object({
   word: z.string(),
   definition: z.string(),
--- validations/game-data/types.ts
+++ validations/game-data/types.ts
@@ -1,17 +1,4 @@
 import z from "zod";
-import {
-  GameDataSchema,
-  GameDataSchemaWithWords,
-  GameFighterSchema,
-  RoundsSchema,
-  RoundsSchemaWithWords,
-  WordSchema,
-} from "./schema";
-export type GameDataSchemaType = z.infer<typeof GameDataSchema>;
+import { GameFighterSchema, WordSchema } from "./schema";
 export type GameFighterSchemaType = z.infer<typeof GameFighterSchema>;
-export type RoundsSchemaType = z.infer<typeof RoundsSchema>;
-export type RoundsSchemaTypeWithWords = z.infer<typeof RoundsSchemaWithWords>;
-export type GameDataSchemaTypeWithWords = z.infer<
-  typeof GameDataSchemaWithWords
->;
 export type WordSchemaType = z.infer<typeof WordSchema>;
