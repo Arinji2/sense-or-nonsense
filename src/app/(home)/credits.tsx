658f1caa544ac7785905b1e4a04ffba60f4d892a
Arinji2 <arinjaydhar205@gmail.com>
2024-09-30 13:57:31 +0530

    Finish Credits

--- src/app/(home)/credits.tsx
+++ src/app/(home)/credits.tsx
@@ -4,19 +4,19 @@ export default function Credits() {
   return (
     <div className="z-10 h-fit w-full bg-[#2C2828] xl:h-[100svh]">
       <div className="flex aspect-square h-auto w-full flex-col items-center justify-start gap-10 rounded-b-full bg-gradient-to-b from-[#EC4899]/25 to-[#1E1E1E]/25 px-4 py-10 md:pb-40 xl:aspect-auto xl:h-full xl:pb-32">
-        <h3 className="text-[30px] font-bold tracking-subtitle text-white md:text-[50px] xl:text-[60px]">
+        <h3 className="text-2xl font-bold tracking-title text-white md:text-5xl xl:text-6xl">
           CREDITS
         </h3>
-        <p className="max-w-[560px] text-center text-[12px] font-medium tracking-text text-white/70 md:text-[20px]">
+        <p className="text-xss max-w-[700px] text-center text-white/70 md:text-lg">
           This site would not have been possible without the wonderful people
           who helped :D
         </p>
         <div className="mt-auto flex h-fit flex-col items-center justify-center gap-5">
-          <p className="text-center text-[12px] font-bold tracking-subtitle text-purple-500 md:text-[25px] xl:text-[30px]">
+          <p className="text-center text-xs font-bold tracking-title text-purple-500 md:text-xl xl:text-2xl">
             FROM FIGHTER LORE TO THE SOUNDTRACKS
           </p>
           <Button className="bg-purple-500 px-3 py-2">
-            <p className="text-xs font-bold tracking-text text-white xl:text-sm">
+            <p className="whitespace-nowrap text-xs font-bold text-white">
               SHOW ME MORE!
             </p>
           </Button>
